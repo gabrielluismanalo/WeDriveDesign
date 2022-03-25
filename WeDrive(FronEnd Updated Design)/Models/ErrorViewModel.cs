e1b77f7769832162aa013fc6256a658dc9db8e00
Gabriel Luis Manalo <11704216@benilde.edu.ph>
2022-03-25 10:03:18 +0800

    Initial commit

---
+++ WeDrive(FronEnd Updated Design)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WeDrive_FronEnd_Updated_Design_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
